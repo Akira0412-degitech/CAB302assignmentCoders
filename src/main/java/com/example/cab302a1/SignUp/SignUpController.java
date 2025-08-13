1acbbb8cbece024361dea0c7cc44e2f9c50a8dce
Akira Hasuo <akira.degitech@gmail.com>
2025-08-13 18:14:38 +1000

    added fields on signup controller

--- src/main/java/com/example/cab302a1/SignUp/SignUpController.java
+++ src/main/java/com/example/cab302a1/SignUp/SignUpController.java
@@ -1,12 +1,18 @@
 package com.example.cab302a1.SignUp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class SignUpController   {
+    @FXML
     private TextField usernameField;
 
     @FXML
     private PasswordField passwordField;
+
+    @FXML
+    private ComboBox<String> selectiveField;
+
 }
