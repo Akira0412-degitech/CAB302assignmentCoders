2a96ca966a426c44f31e2203254ea517ef258507
Akira Hasuo <akira.degitech@gmail.com>
2025-08-13 17:04:59 +1000

    Signup? button on loginpage implemented

--- src/main/java/com/example/cab302a1/Login/LoginController.java
+++ src/main/java/com/example/cab302a1/Login/LoginController.java
@@ -1,8 +1,13 @@
 package com.example.cab302a1.Login;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class LoginController {
 
@@ -13,6 +18,8 @@ public class LoginController {
     private PasswordField passwordField;
 
     @FXML
+    private Hyperlink signUpLink;
+    @FXML
     protected void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
@@ -22,5 +29,9 @@ public class LoginController {
         } else {
             System.out.println("Invalid credentials.");
         }
+    }
+    @FXML
+    private void handleSignUpClick (){
+        System.out.print("Signup button clicked");
     }
 }
