39682cfe8132b60790857ad35402821f2aea40d7
Akira Hasuo <akira.degitech@gmail.com>
2025-08-15 18:33:13 +1000

    Change the window size on signup page

--- src/main/java/com/example/cab302a1/Login/LoginController.java
+++ src/main/java/com/example/cab302a1/Login/LoginController.java
@@ -37,7 +37,8 @@ public class LoginController {
     @FXML
     private void handleSignUpClick (ActionEvent event) throws IOException{
         root = FXMLLoader.load(getClass().getResource("/com/example/cab302a1/SignUp/SignUp-view.fxml"));
-        scene = new Scene(root);
+        scene = new Scene(root, 320, 240);
+
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle("SignUp");
         stage.setScene(scene);
