4e8960d46eb5ca257d67abf9623b74dfad5b3e92
Akira Hasuo <akira.degitech@gmail.com>
2025-08-15 18:53:35 +1000

    Rolebox implemented

--- src/main/java/com/example/cab302a1/SignUp/SignUpController.java
+++ src/main/java/com/example/cab302a1/SignUp/SignUpController.java
@@ -1,11 +1,22 @@
 package com.example.cab302a1.SignUp;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class SignUpController   {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private TextField usernameField;
 
@@ -13,6 +24,23 @@ public class SignUpController   {
     private PasswordField passwordField;
 
     @FXML
-    private ComboBox<String> selectiveField;
+    private ChoiceBox roleBox;
 
+    @FXML
+    private void initialize() {
+        roleBox.getItems().addAll("Teacher", "Student");
+
+        roleBox.setValue("Student");
+    }
+
+    @FXML
+    private void handleLoginClick (ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/com/example/cab302a1/SignUp/Login-view.fxml"));
+        scene = new Scene(root, 320, 240);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
