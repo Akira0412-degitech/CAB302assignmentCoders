45f9198fbed0770c0e2726c7b72c91c93da17eb3
Akira Hasuo <akira.degitech@gmail.com>
2025-09-13 17:59:52 +1000

    startAttempt method implemented

--- src/main/java/com/example/cab302a1/dao/AttemptDao.java
+++ src/main/java/com/example/cab302a1/dao/AttemptDao.java
@@ -1,8 +1,31 @@
 package com.example.cab302a1.dao;
 
-import java.sql.Time;
-import java.sql.Timestamp;
+import com.example.cab302a1.DBconnection;
+import com.example.cab302a1.model.Quiz;
+
+import java.sql.*;
 
 public class AttemptDao {
+
+    public int startAttempt(int _quiz_id, int _user_id){
+        String sql = "INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed) VALUES(?, ?, 0, 0)";
+
+        try(Connection conn = DBconnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)){
+            pstmt.setInt(1, _quiz_id);
+            pstmt.setInt(2, _user_id);
+            pstmt.executeUpdate();
+
+            try(ResultSet rs = pstmt.getGeneratedKeys()){
+                if(rs.next()){
+                    return rs.getInt(1);
+                }
+            }
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
 
 }
