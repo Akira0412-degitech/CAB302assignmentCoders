36a5e95057623b3f3b7b93780af333cecc99c9e6
Akira Hasuo <akira.degitech@gmail.com>
2025-09-16 18:10:44 +1000

    V11 inserting default data for quesion_responses implemented

--- src/main/resources/db/migration/V10__insert_quiz_attempts_defaultValue.sql
+++ src/main/resources/db/migration/V10__insert_quiz_attempts_defaultValue.sql
@@ -1,23 +1,23 @@
 
 -- Default Data for quiz_attempts table
 
--- testuser@example.com tried quiz_id = 1 and the score is 16
+-- testuser@example.com tried quiz_id = 1 and the score is 3
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
-VALUES(1, 3, 16, TRUE);
--- quiz_id = 2 and the score is 7
+VALUES(1, 3, 3, TRUE);
+-- quiz_id = 2 and the score is 2
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
-VALUES(2, 3, 7, TRUE);
--- quiz_id = 3 and the score is 31
+VALUES(2, 3, 2, TRUE);
+-- quiz_id = 3 and yet completed
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
 VALUES(3, 3, null, false);
 
 -- demo@example.com tried
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
--- quiz_id = 1 and the score is 9
+-- quiz_id = 1 and yet completed
 VALUES(1, 1, null, false);
--- quiz_id = 2 and the score is 3
+-- quiz_id = 2 and the score is 0
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
-VALUE(2, 1, 3, TRUE);
+VALUE(2, 1, 0, TRUE);
 -- quiz_id = 3 and the score is 2
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
 VALUES(3, 1, 2, TRUE);
