0531e0d4f3e40e04b17df0502a40ce94391e838d
Akira Hasuo <akira.degitech@gmail.com>
2025-09-16 10:32:18 +1000

    V10 deleted

--- src/main/resources/db/migration/V10__remove_username_unique_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Remove UNIQUE constraint from username column
--- Username should not be unique, only email should be unique
-
--- Drop the unique constraint on username
-ALTER TABLE users DROP CONSTRAINT uk_users_username;
