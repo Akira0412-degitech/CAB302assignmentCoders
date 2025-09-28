8f9dc119c704c2590d0352c731db1e9db7e3b429
Jereban4 <jbunn0421@gmail.com>
2025-09-28 22:09:13 +1000

    HomePage refactoring test roll back

--- src/main/java/com/example/cab302a1/HomePage.java
+++ src/main/java/com/example/cab302a1/HomePage.java
@@ -1,33 +1,33 @@
-//package com.example.cab302a1;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//import java.util.Objects;
-//import com.example.cab302a1.components.NavigationManager;
-//public class HomePage extends Application {
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        FXMLLoader loader = new FXMLLoader(
-//                Objects.requireNonNull(getClass().getResource("/com/example/cab302a1/HomePage.fxml"))
-//        );
-//
-//        Scene scene = new Scene(loader.load(), 1000, 640);
-//        var css = getClass().getResource("/com/example/cab302a1/HomePage.css");
-//
-//        if (css != null) scene.getStylesheets().add(css.toExternalForm());
-//
-//        stage.setTitle("Quiz App");
-//        stage.setWidth(1000);
-//        stage.setHeight(600);
-//
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
+package com.example.cab302a1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+import com.example.cab302a1.components.NavigationManager;
+public class HomePage extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                Objects.requireNonNull(getClass().getResource("/com/example/cab302a1/HomePage.fxml"))
+        );
+
+        Scene scene = new Scene(loader.load(), 1000, 640);
+        var css = getClass().getResource("/com/example/cab302a1/HomePage.css");
+
+        if (css != null) scene.getStylesheets().add(css.toExternalForm());
+
+        stage.setTitle("Quiz App");
+        stage.setWidth(1000);
+        stage.setHeight(600);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
