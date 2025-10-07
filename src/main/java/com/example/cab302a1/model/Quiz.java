481dec92711965db5bda133a19048cd4c14f3f45
Akira Hasuo <akira.degitech@gmail.com>
2025-10-08 01:37:27 +1000

    Quiz class updated

--- src/main/java/com/example/cab302a1/model/Quiz.java
+++ src/main/java/com/example/cab302a1/model/Quiz.java
@@ -10,6 +10,8 @@ public class Quiz {
     private String title;
     private String description;
     private  int created_by;
+    private boolean is_Hidden;
+
     private final List<QuizQuestionCreate> questions = new ArrayList<>();
 
     public Quiz(int quiz_id, String title, String description, int created_by) {
@@ -17,6 +19,7 @@ public class Quiz {
         this.title = title;
         this.description = description;
         this.created_by = created_by;
+        this.is_Hidden = false;
     }
 
     public Quiz(){
@@ -27,13 +30,16 @@ public class Quiz {
     public String getTitle() { return title; }
     public String getDescription() { return description; }
     public int getCreated_by(){ return created_by;}
+    public boolean getIsHidden(){return is_Hidden;}
     public List<QuizQuestionCreate> getQuestions() { return questions; }
 
     public void setQuizId(Integer _quizId){this.quiz_id = _quizId;}
     public void setTitle(String title) { this.title = title; }
     public void setDescription(String description) { this.description = description; }
     public void setCreated_by(Integer _created_by){this.created_by = _created_by;}
-
+    public void setIsHidden(boolean _is_Hidden){
+        this.is_Hidden = _is_Hidden;
+    }
 
     public void setQuestions(List<QuizQuestionCreate> qs) {
         this.questions.clear();
