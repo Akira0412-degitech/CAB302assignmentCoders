f8ba8b104f4717d21aa0c3202146cc764ca1e51f
Akira Hasuo <akira.degitech@gmail.com>
2025-10-14 15:22:40 +1000

    delete V14 and rename V15 V14

--- src/main/resources/db/migration/V10__insert_quiz_attempts_defaultValue.sql
+++ src/main/resources/db/migration/V10__insert_quiz_attempts_defaultValue.sql
@@ -9,12 +9,12 @@ INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
 VALUES(2, 3, 2, TRUE);
 -- quiz_id = 3 and yet completed
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
-VALUES(3, 3, null, false);
+VALUES(3, 3, null, TRUE);
 
 -- demo@example.com tried
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
 -- quiz_id = 1 and yet completed
-VALUES(1, 1, null, false);
+VALUES(1, 1, null, TRUE);
 -- quiz_id = 2 and the score is 0
 INSERT INTO quiz_attempts (quiz_id, answered_by, score, is_completed)
 VALUE(2, 1, 0, TRUE);
